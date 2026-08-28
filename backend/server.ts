94053d02bab4fd112c57fc309ee9205d93b7e4b5
idolast26-tech <idolast26@gmail.com>
2026-08-28 07:38:55 +0700

    Update server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -34,7 +34,7 @@ app.use(cors({
   },
   credentials: true,
 }));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 app.use('/api', apiRoutes);
 app.use('/api/admin', adminRouter);
